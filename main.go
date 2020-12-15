13051264ded27b8295ec16b668db6b56142955d8
Gianni Salinetti <gsalinet@redhat.com>
2020-12-15 08:55:51 +0100

    Richer http print and readiness probes

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello OpenShift!\n")
+func exampleServer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<html>\n<body>\n"))
+	w.Write([]byte("<p>Hello World!</p>\n"))
+	w.Write([]byte("</body>\n</html>\n"))
+}
+
+func readinessProbe(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("OK\n"))
 }
 
 func livenessProbe(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Health OK")
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("OK\n"))
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/healthz", livenessProbe)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", exampleServer)
+	http.HandleFunc("/ready", readinessProbe)
+	http.HandleFunc("/alive", livenessProbe)
+	log.Println("Starting http server")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
